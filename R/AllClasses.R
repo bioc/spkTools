65e53c6f3b44e7c2d48c100f377eef0f5f2dc82c
Matthew McCall <mmccall@jhsph.edu>
2008-10-14 18:21:48 +0000

    warnings fixed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/spkTools@34483 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,20 +1,2 @@
 ## define our new class: SpikeInExpressionSet
-require(Biobase)
 setClass("SpikeInExpressionSet", contains="ExpressionSet")
-setMethod("initialize", "SpikeInExpressionSet",
-          function(.Object,
-                   assayData = assayDataNew(exprs=exprs, spikeIn=spikeIn, ...),
-                   exprs=new("matrix"),
-                   spikeIn=new("matrix"),
-                   phenoData = annotatedDataFrameFrom  (assayData, byrow=FALSE),
-                   featureData = annotatedDataFrameFrom(assayData, byrow=TRUE),
-                   experimentData = new("MIAME"),
-                   annotation = new("character"), ...){
-            .Object <- callNextMethod(.Object,
-                                      assayData = assayData,
-                                      phenoData = phenoData,
-                                      experimentData = experimentData,
-                                      annotation = annotation,
-                                      featureData = featureData)
-            .Object
-          })
