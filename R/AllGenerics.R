66a55119fa64f3d6e1df0761a101d35df8646ed0
Matthew McCall <mmccall@jhsph.edu>
2008-12-09 21:25:46 +0000

    adding createSpikeInExpressionSet and spell checked
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/spkTools@35796 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,4 +1,6 @@
 ## generic functions
 setGeneric("spikeIn", function(object) standardGeneric("spikeIn"))
 setGeneric("spkSplit", function(object) standardGeneric("spkSplit"))
-
+setGeneric("spikeIn<-", function(object, value) standardGeneric("spikeIn<-"))
+setGeneric("createSpikeInExpressionSet",
+           function(object, exprs, spikeIn, ...) standardGeneric("createSpikeInExpressionSet"))
